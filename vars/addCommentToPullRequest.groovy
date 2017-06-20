6d9f9bdbb2aaf4826569f9f16f6d55a1cb6a49e8
Joost VanDorp <joostvandorp@gmail.com>
2017-06-20 13:59:17 -0400

    Add first libraries to project

---
+++ vars/addCommentToPullRequest.groovy
@@ -0,0 +1,36 @@
+#!/usr/bin/groovy
+
+/**
+ * Adds a comment on the PR in github
+ * @param comment - The comment to leave in the comment section. Should not be empty.
+ * @param pr - The pull-request id, should be int.
+ * @param project - The github project , should be string.
+ */
+def call(comment, pr, project) {
+    def githubToken = getGitHubToken()
+    def apiUrl = new URL("https://api.github.com/repos/${project}/issues/${pr}/comments")
+    echo "adding ${comment} to ${apiUrl}"
+    try {
+        def HttpURLConnection connection = apiUrl.openConnection()
+        if (githubToken.length() > 0) {
+            connection.setRequestProperty("Authorization", "Bearer ${githubToken}")
+        }
+        connection.setRequestMethod("POST")
+        connection.setDoOutput(true)
+        connection.connect()
+
+        def body = "{\"body\":\"${comment}\"}"
+
+        OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream())
+        writer.write(body)
+        writer.flush()
+
+        // execute the POST request
+        new InputStreamReader(connection.getInputStream())
+
+        connection.disconnect()
+    } catch (err) {
+        error "ERROR  ${err}"
+    }
+}
+
