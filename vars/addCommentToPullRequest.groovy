7977c88dc0938bcc3d2cf49003a206ac0d5e005a
Joost VanDorp <joostvandorp@gmail.com>
2017-06-20 16:56:53 -0400

    Do not error if PR is not supplied. Instead just log a message

--- vars/addCommentToPullRequest.groovy
+++ vars/addCommentToPullRequest.groovy
@@ -7,32 +7,36 @@
  * @param project - The github project , should be string.
  */
 def call(comment, pr, project) {
-    withCredentials([[$class: 'StringBinding', credentialsId: 'github_oath_token', variable: 'GITHUB_ACCESS_TOKEN']]) {
-        def githubToken = "${GITHUB_ACCESS_TOKEN}"
-        def apiUrl = new URL("https://api.github.com/repos/${project}/issues/${pr}/comments")
-        echo "adding ${comment} to ${apiUrl}"
-        try {
-            def HttpURLConnection connection = apiUrl.openConnection()
-            if (githubToken.length() > 0) {
-                connection.setRequestProperty("Authorization", "Bearer ${githubToken}")
+    if(pr) {
+        withCredentials([[$class: 'StringBinding', credentialsId: 'github_oath_token', variable: 'GITHUB_ACCESS_TOKEN']]) {
+            def githubToken = "${GITHUB_ACCESS_TOKEN}"
+            def apiUrl = new URL("https://api.github.com/repos/${project}/issues/${pr}/comments")
+            echo "adding ${comment} to ${apiUrl}"
+            try {
+                def HttpURLConnection connection = apiUrl.openConnection()
+                if (githubToken.length() > 0) {
+                    connection.setRequestProperty("Authorization", "Bearer ${githubToken}")
+                }
+                connection.setRequestMethod("POST")
+                connection.setDoOutput(true)
+                connection.connect()
+
+                def body = "{\"body\":\"${comment}\"}"
+
+                OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream())
+                writer.write(body)
+                writer.flush()
+
+                // execute the POST request
+                new InputStreamReader(connection.getInputStream())
+
+                connection.disconnect()
+            } catch (err) {
+                error "ERROR  ${err}"
             }
-            connection.setRequestMethod("POST")
-            connection.setDoOutput(true)
-            connection.connect()
-
-            def body = "{\"body\":\"${comment}\"}"
-
-            OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream())
-            writer.write(body)
-            writer.flush()
-
-            // execute the POST request
-            new InputStreamReader(connection.getInputStream())
-
-            connection.disconnect()
-        } catch (err) {
-            error "ERROR  ${err}"
         }
+    } else {
+        echo "Did not receive pr value! Comment: ${comment} not posted"
     }
 }
 
